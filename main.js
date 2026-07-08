16d666182e73c32333d3f30ff3930562d886a651
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2026-07-08 18:16:41 -0300

    .

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const forcaSenha = document.querySelector('.forca');
 
 function classificaSenha(tamanhoAlfabeto){
     const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent = Math.floor(entropia)/(100e6*60*60*24);
+    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
     let entropia = tamanhoSenha * Math.log2(tamanhoSenha);
     console.log(entropia);
     forcaSenha.classList.remove('fraca','media','forte');
