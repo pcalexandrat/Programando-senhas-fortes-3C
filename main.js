6e327a60b9e54ba6f1a4b4fc6ac8a5a606948bc9
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2026-07-08 17:48:11 -0300

    .

--- main.js
+++ main.js
@@ -51,14 +51,12 @@ botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho(){
     if (tamanhoSenha > 1){
-       // tamanhoSenha = tamanhoSenha-1;
         tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
 }
 function aumentaTamanho(){
     if (tamanhoSenha < 20){
-       //tamanhoSenha = tamanhoSenha+1;
        tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
