3057a347237302eae7bed71bb23439a25a2f774d
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2026-07-08 18:27:33 -0300

    .

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
-let tamanhoSenha = 12;
+let tamanhoSenha = 5;
 numeroSenha.textContent = tamanhoSenha;
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
 const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
