9c2dcd80883ad946f13c47139f6afcb0698690dc
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2026-07-09 08:07:21 -0300

    .

--- main.js
+++ main.js
@@ -67,9 +67,9 @@ function classificaSenha(tamanhoAlfabeto){
     let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
     console.log(entropia);
     forcaSenha.classList.remove('fraca','media','forte');
-    if (entropia > 20){
+    if (entropia > 12){
         forcaSenha.classList.add('forte');
-    } else if (entropia > 5 && entropia < 20) {
+    } else if (entropia > 5 && entropia < 12) {
         forcaSenha.classList.add('media');
     } else if (entropia <= 5){
         forcaSenha.classList.add('fraca');
